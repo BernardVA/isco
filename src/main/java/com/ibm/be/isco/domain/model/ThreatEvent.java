d86d491e94ed07ae1b271e07a9df49cacccc89ca
Bernard van Acker <bernard_vanacker@be.ibm.com>
2025-10-01 18:17:24 +0200

    3rd upload 1 Oct 2025

--- src/main/java/com/ibm/be/isco/domain/model/ThreatEvent.java
+++ src/main/java/com/ibm/be/isco/domain/model/ThreatEvent.java
@@ -1,7 +1,7 @@
 package com.ibm.be.isco.domain.model;
 
 import jakarta.persistence.Column;
-//1. Represent database tables and are managed by JPA/Hibernate.
+/* 1. Represent database tables and are managed by JPA/Hibernate.*/
 public class ThreatEvent {
     @Column(name = "Strength", nullable = false)
 private String strength;
--- src/main/java/com/ibm/be/isco/domain/repository/ControlRepository.java
+++ src/main/java/com/ibm/be/isco/domain/repository/ControlRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-//2. Provide CRUD operations for entities.
+//2. Provide CRUD operations for entities, here controls.
 public interface ControlRepository extends JpaRepository<Control, Long> {
 }
